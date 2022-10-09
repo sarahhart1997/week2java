432ba683d48007ba79241354ac5555759adcf94c
Sarah Hart <sarah.hart.nc@gmail.com>
2022-10-09 15:48:42 -0400

    base instructions

---
+++ main.java
@@ -0,0 +1,23 @@
+// Design a window interace
+// Light shade as a background color 
+
+// Window has 3 text fields
+
+// Window has 4 radio buttons (+, -, *, /)
+
+// A label for the equality sign
+
+// User enters 2 integers in the text fields
+
+// Click on any of the radio buttons
+
+// Result appears in the 3rd text field 
+// Result should be usually non-integer + handled correctly
+// Try catch block for the following
+// Non integer inputs in one or both text fields
+// No input in one or both text fields
+// Division by zerio 
+
+
+// Notes convert both text fields to integer (integer class)
+// Your program must issue an error message by a dialog box
