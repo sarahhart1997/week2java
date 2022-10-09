dc7c79c207e272299c807cc8b80db50ec62345cd
Sarah Hart <sarah.hart.nc@gmail.com>
2022-10-09 19:44:18 -0400

    radio button updates

--- main.java
+++ main.java
@@ -1,10 +1,48 @@
-// Design a window interace
+// Imports
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ButtonGroup;
+import javax.swing.Jbutton;
+import javax.swing.JRadioButton;
+import javax.swing.WindowConstants;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
+import javax.swing.SwingUtilities; 
+
+// Design a window interface
 // Light shade as a background color 
 
 // Window has 3 text fields
 
 // Window has 4 radio buttons (+, -, *, /)
+public class RadioButtons extends javax.swing.JFrame {
+    private JRadioButton jRadioButtonPlus;
+    private JRadioButton jRadioButtonSub;
+    private JRadioButton jRadioButtonMulti;
+    private JRadioButton jRadioButtonDiv;
+    private JButton jButtonPlus;
+    private JButton jButtonSub;
+    private JButton jButtonMulti;
+    private JButton jButtonDiv;
+    private ButtonGroup buttonGroup;
 
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                RadioButtons inst = new RadioButtons();
+                inst.setLocationRelativeTo(null);
+                inst.setVisible(true);
+            }
+        });
+    }
+
+    public RadioButtons(){
+        super();
+        initGUI();
+    }
+
+    
+}
 // A label for the equality sign
 
 // User enters 2 integers in the text fields
